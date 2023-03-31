5a820854fa7ca56f0f6867d3edb52b0b80fa42a1
soldesk <soldesk@DESKTOP-T1BMDAM>
2023-03-31 15:11:30 +0900

    2023_03_Spring

---
+++ spring_basic/src/main/java/kr/co/softsoldesk/beans/SamsungTV.java
@@ -0,0 +1,34 @@
+package kr.co.softsoldesk.beans;
+
+public class SamsungTV implements TV{
+	
+	
+	public SamsungTV() {
+		System.out.println("===> SamsungTV 객체 생성");
+	}
+
+	@Override
+	public void powerOn() {
+		System.out.println("SamsungTV 켜기");
+		
+	}
+
+	@Override
+	public void powerOff() {
+		System.out.println("SamsungTV 전원끄기 ");
+		
+	}
+
+	@Override
+	public void volumeUp() {
+		System.out.println("===> SamsungTV 볼륨업");
+		
+	}
+
+	@Override
+	public void volumeDown() {
+		System.out.println("===> SamsungTV 볼륨 다운");
+		
+	}
+     
+}
